0e76fdbf9271219df78e833bdfb0f10f3cb49ae2
Mohammad Shafi <bhatrameez2009@gmail.com>
2026-08-28 10:27:03 +0530

    speed

--- usmlesarthi/src/app/sarthi-list/services/sarthi-list.service.ts
+++ usmlesarthi/src/app/sarthi-list/services/sarthi-list.service.ts
@@ -149,10 +149,9 @@ export class SarthiListService {
       historyByProgram[key].push(data);
 
       // Query is newest first, so the first record is the latest.
-      /*if (!latestByProgram[key]) {
+      if (!latestByProgram[key]) {
         latestByProgram[key] = data;
-      }*/
-      latestByProgram[key] = data;
+      }
     }
 
     this.hospitalsDataByHPId = {
